6e7f96b644cac2177f4aee8fda602234aa33623a
pamellaneco <pamellakyrlatech@gmail.com>
2023-03-29 19:43:26 -0300

    tcc-007 iniciando perfil do cliente - mesmo modelo do agricultor

--- laravel-app/resources/views/dashboardConsumer.blade.php
+++ laravel-app/resources/views/dashboardConsumer.blade.php
@@ -7,14 +7,33 @@
         background-size: cover;
     }
 </style>
-<div class="container mt-5">
-    <div class="row mt-5 justify-content-center">
-        <div class="col-md-6">
-            <div class="card mt-5 bg-success text-white">
-                <div class="card-header">Bem vindo à sua conta, {{ auth()->user()->name }}</div>
-
-                <div class="card-body">
-                    <h1>Consumidor</h1>
+<div class="row col-md-12 align-content-center justify-content-center">
+    <div class="container col-md-4 ">
+        <div id="container" class="row col-md-12 justify-content-center align-content-center text-align-center">
+            <div class="row col-md-12">
+                <div class= "col-md-12 mt-3 card bg-success text-white text-align-center">
+                    <div class="card-header">Perfil - {{ auth()->user()->tipoUsuario }}</div>
+    
+                    <div class="card-body">
+                        <img src="/img/user.png" alt="foto de perfil" width="30%">
+                        <h4 class="mt-3">Nome: </h4>
+                        {{ auth()->user()->name }}
+                        <h4 class="mt-3">Email: </h4> 
+                        {{ auth()->user()->email }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container col-md-8">
+        <div id="container" class="row col-md-12 justify-content-center align-content-center text-align-center">
+            <div class="row col-md-12">
+                <div class= "col-md-12 mt-3 card bg-success text-white">
+                    <div class="card-header">Página Inicial</div>
+    
+                    <div class="card-body">
+                    Bem vindo à sua conta, {{ auth()->user()->name }}
+                    </div>
                 </div>
             </div>
         </div>

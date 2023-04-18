98ad33d32b6f327326e416d7bb4014573847ff11
pamellaneco <pamellakyrlatech@gmail.com>
2023-04-18 09:22:24 -0300

    tcc-008.2 ajustando formulário de publicações de ofertas

--- laravel-app/resources/views/postsPage/create.blade.php
+++ laravel-app/resources/views/postsPage/create.blade.php
@@ -5,16 +5,20 @@
 <div class="container align-content-center justify-content-center">
     <div id="container1" class="align-content-center justify-content-center">
         <div class="col-md-12">
-            <div id="teste" class="text-center p-3 w-20 text-white text-bold">
+            <div id="banner" class="text-center p-3 w-20 text-white text-bold">
                 <h1>Criar publicação</h1>
                 <h2>Insira aqui as informações para publicar:</h2>
             <style>
-                #teste {
+                #banner {
                     background-image: url('/img/image4.jpg');
+                    background-size:cover ;
                 }
-                #teste2{
+                #form-card {
+                    background-image: url('/img/image4.jpg');
+                    background-size:cover ;
                     display: flex;
-                    flex-direction: row;
+                    align-items: center;
+                    justify-content: center;
                 }
                 #form-posts {
                     display: flex;
@@ -27,11 +31,11 @@
                 <div class="container mt-5">
                     <div class="row mt-5 justify-content-center">
                         <div class="col-md-6">
-                            <div class="card mt-5 bg-success text-white">
+                            <div id="form-card" class="card mt-5 bg-success text-white">
                                 <div class="card-header">{{ __('Entre na sua conta:') }}</div>
 
                                 <div class="card-body">
-                                    <form class="col-md-8" id="form-posts" action="/postsPage" method="POST" enctype="multipart/form-data">
+                                    <form class="col-md-12" id="form-posts" action="/postsPage" method="POST" enctype="multipart/form-data">
                                         @csrf
                                         <div class="m-1">
                                             <label class="mt-2 col-md-8 col-form-label text-md-end" for="title">{{ __('Título:') }}</label>

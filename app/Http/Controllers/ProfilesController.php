e724cd836803b879c8398acef62595fe9ae7f502
pamellaneco <pamellakyrlatech@gmail.com>
2023-05-29 13:57:53 -0300

    review - apagando quebras de linha e códigos comentados de ProfilesController

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -11,11 +11,9 @@ class ProfilesController extends Controller
 {
     public function listProducersProfiles()
     {
-
         $tipoUsuario = "agricultor";
         $usuarios = User::where('tipoUsuario', $tipoUsuario);
         return view ('listProducersProfiles')->with('usuarios', $usuarios->get());
-
     }
     
     public function showEditForm($id) //aqui é pra levar ao form de edição
@@ -23,26 +21,18 @@ class ProfilesController extends Controller
         $variavel_com_dados_do_banco = User::find($id);
         $atributos_do_banco = $variavel_com_dados_do_banco ->getAttributes();
         return view ('updateUserProfile')->with('perfil', $atributos_do_banco);
-        
     }
 
     public function edit($id, Request $request) //aqui é pra editar
     {
-        /*
-        $variavel_com_dados_do_banco = User::find($id);
-        $atributos_do_banco = $variavel_com_dados_do_banco ->getAttributes();
-        */
-       
         User::where('id', $id)->update([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'place' => $request->input('place'),
             'phone' => $request->input('phone'),
-           // 'user_id' => auth()->user()->id
         ]);
 
         return redirect('/profile')->with('message', 'Seu perfil foi atualizado com sucesso!');
-
     }
 
 }
